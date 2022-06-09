ffeec3378510050520cee2e9ba14196bc4984df1
Jerry Zhang <jerryzhang101@yahoo.com>
2022-06-09 10:36:14 -0400

    Added basic content and styles for HomeSplash

--- src/components/molecules/HomeSplash.js
+++ src/components/molecules/HomeSplash.js
@@ -7,10 +7,10 @@ const HomeSplash = () => {
     return(
         <section className='splash-section'>
             <article className='splash-article'>
-                <h2 className='splash-title'>
+                <h2 className='splash-title header-font'>
                     Welcome to my Blog
                 </h2>
-                <p className='splash-content'>
+                <p className='splash-content content-font'>
                     Check out how my mind works
                 </p>
             </article>
