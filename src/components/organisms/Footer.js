841712eb2e486fb82a846d5803107e86ea8befbd
Jerry Zhang <jerryzhang101@yahoo.com>
2022-06-13 19:08:32 -0400

    Added footer and footer links

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -5,8 +5,9 @@ import './organisms.css';
 // Component that is being exported
 const Footer = () => {
     return(
-        <footer>
-
+        <footer className='site-footer' >
+            <h3>Check out my github &#8594;<a target='_blank' className='footer-link github' href='https://www.github.com/Jerriath' ></a>&#8592;</h3>
+            <h3>Check out the repo &#8594;<a target='_blank' className='footer-link' href='https://www.github.com/Jerriath/blog-client' >😀</a>&#8592;</h3>
         </footer>
     );
 };
